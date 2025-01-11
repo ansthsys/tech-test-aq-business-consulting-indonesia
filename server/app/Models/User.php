8e05c31403020e28bcbaf04458a2cbe7f974e859
ansthsys <azmiythufail91@gmail.com>
2025-01-11 11:46:53 +0700

    feat(server): add data to bearer token response

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -51,7 +51,10 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
     }
 
     public function todoLists(): HasMany
